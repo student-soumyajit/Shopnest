c8e7bc0acfbe7b1c2dd32bebec333e20f9153845
soumyajitmalakar@gmail.com <soumyajitmalakar@gmail.com>
2026-08-15 23:35:09 +0530

    added

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,10 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
